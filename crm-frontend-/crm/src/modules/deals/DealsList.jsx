e1e894c9e2befc2d942107b88884d7ce6b1ab2fc
fathima hiba <fathimahibakarumbil@gmail.com>
2026-07-25 12:40:28 -0700

    Fix AppSnackbar import case

--- crm-frontend-/crm/src/modules/deals/DealsList.jsx
+++ crm-frontend-/crm/src/modules/deals/DealsList.jsx
@@ -6,7 +6,7 @@ import ModuleTopBar from "../../components/listPage/ModuleTopBar";
 import DataTable from "../../components/listPage/DataTable";
 import CreateEditDrawer from "../../components/listPage/drawers/CreateEditDrawer";
 import ConfirmationDialog from "../../components/common/ConfirmationDialog";
-import AppSnackbar from "../../components/common/AppSnackBar";
+import AppSnackbar from "../../components/common/AppSnackbar";
 import { useMemo } from "react";
 import { fetchLeads } from "../../store/LeadSlice";
 import {
